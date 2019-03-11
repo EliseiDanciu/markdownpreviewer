fe7d32199d787e124cfec9beafc11f4db5be365f
Elisei Danciu <elisei.danciu@senseisrl.it>
2019-03-11 12:13:25 +0100

    Updates

---
+++ precache-manifest.dce68c7a39127a750965e651433f5ee6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c3dc5e414e4ccf6c02e",
+    "url": "/markdownpreviewer/static/css/main.fd94e9cf.chunk.css"
+  },
+  {
+    "revision": "0c3dc5e414e4ccf6c02e",
+    "url": "/markdownpreviewer/static/js/main.7fd2f893.chunk.js"
+  },
+  {
+    "revision": "e2f8873a24ec4abc4ea9",
+    "url": "/markdownpreviewer/static/js/runtime~main.3825513d.js"
+  },
+  {
+    "revision": "55f09740c68673af1396",
+    "url": "/markdownpreviewer/static/js/2.a4f17275.chunk.js"
+  },
+  {
+    "revision": "692f327443617401aac4070e530e4e96",
+    "url": "/markdownpreviewer/index.html"
+  }
+];
